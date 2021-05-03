4dc25deb061688220fdbfde6a050a29563a9d462
JennyViannay <jenny.viannay75@gmail.com>
2021-05-03 13:29:07 +0200

    add phpunit

---
+++ src/tests/ArticleControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class AdminControllerTest extends TestCase
+{
+    public function testIndex()
+    {
+        $this->assertEquals(false, false);
+    }
+}
